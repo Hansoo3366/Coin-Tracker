98b03a39f99246a313632bf839cbbf0408adb1db
Hansoo3366 <awaol200@naver.com>
2022-03-02 16:09:11 +0900

    First commit of Coin Traker that have coin & coin list page

---
+++ src/Router.tsx
@@ -0,0 +1,18 @@
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Coin from "./routes/Coin";
+import CoinLst from "./routes/CoinLst"
+
+function Router() {
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/:coinId">
+                <Coin />
+            </Route>
+            <Route path="/">
+                <CoinLst />
+            </Route>
+        </Switch>
+    </BrowserRouter>
+}
+
+export default Router;
